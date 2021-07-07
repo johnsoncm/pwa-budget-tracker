314cf8357886692a0b93eacef2a2796f174bc672
Casey Johnson <johnsoncm3@gmail.com>
2021-07-07 17:58:59 -0400

    fixed faulty connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
+mongoose.createConnection(
   process.env.MONGODB_URI || "mongodb://localhost/budget", 
   {
   useNewUrlParser: true,
